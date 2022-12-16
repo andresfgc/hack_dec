c16d469bafcbaefadf4d869a862649e507206e7f
MichealCWalsh <MichaelCWalsh792@gmail.com>
2022-12-16 16:06:10 +0000

    Add allauth to urls and CSRF for admin

--- dublin_meet_ups/dublin_meet_ups/settings.py
+++ dublin_meet_ups/dublin_meet_ups/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%^*yw%@!1m7l#u!9!eh-u5+a1-dpp(#(oy!st!l44%s(f5q=vl'
+SECRET_KEY = 'django-insecure-%^*yw%@!1m7l#u!9!eh-u5+a1-dpp(#(oy!st!l44%s(f5q=vl'  # noqa E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +79,10 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+SITE_ID = 1
+
 WSGI_APPLICATION = 'dublin_meet_ups.wsgi.application'
+CSRF_TRUSTED_ORIGINS = ['https://8000-michaelcwalsh7-hackdec-kbo67ayod6h.ws-eu79.gitpod.io']  # noqa E501
 
 
 # Database
@@ -98,16 +101,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa E501
     },
 ]
 
