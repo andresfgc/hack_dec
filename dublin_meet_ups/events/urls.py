dd6bb51adc27e5db25158d127eacd59adbe75586
MichealCWalsh <MichaelCWalsh792@gmail.com>
2022-12-17 21:20:46 +0000

    Connect event app to front end

--- dublin_meet_ups/events/urls.py
+++ dublin_meet_ups/events/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('', views.events_list, name='events_list'),
+    path('<str:event_type_arg>', views.events_list, name='events_list'),
     path('event/', views.event, name='event'),
     path('add_event/', views.add_event, name='add_event'),
     path('edit_event/', views.edit_event, name='edit_event'),
--- dublin_meet_ups/events/views.py
+++ dublin_meet_ups/events/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from events.models import Event
 
 
 def event(request):
     return render(request, 'events/event.html')
 
 
-def events_list(request):
-    return render(request, 'events/events-list.html')
+def events_list(request, event_type_arg):
+    if event_type_arg == 'all':
+        events = Event.objects.all()
+        event_type = 'All Events'
+    else:
+        events = Event.objects.all()
+
+    context = {
+        'events': events,
+        'event_type': event_type
+    }
+
+    return render(request, 'events/events-list.html', context)
 
 
 def add_event(request):
