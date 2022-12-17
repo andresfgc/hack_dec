ae5398cc9124e59aa3ec7f4fdc458677458ee1b5
MichealCWalsh <MichaelCWalsh792@gmail.com>
2022-12-17 15:13:51 +0000

    Fix error in Event model

--- dublin_meet_ups/events/models.py
+++ dublin_meet_ups/events/models.py
@@ -9,12 +9,12 @@ class Event(models.Model):
     description = models.TextField()
     date = models.CharField(
         max_length=254, null=True, blank=True,
-        help_text="Please use the following format: <em>DD-MM-YYYY</em>."),
-    category = models.CharField(max_length=254),
-    location = models.CharField(max_length=254, null=True, blank=True),
-    user = models.CharField(max_length=24),
-    people = models.IntegerField(null=True, blank=True),
-    image = models.ImageField(null=True, blank=True),
+        help_text="Please use the following format: <em>DD-MM-YYYY</em>.")
+    category = models.CharField(max_length=254, null=True)
+    location = models.CharField(max_length=254, null=True, blank=True)
+    user = models.CharField(max_length=24, null=True)
+    people = models.IntegerField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     image_url = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
